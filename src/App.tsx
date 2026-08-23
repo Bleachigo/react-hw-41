8805ed1de4af5703c8891d05b66fee0387b2019c
Artem <artwhite2012@gmail.com>
2026-08-23 16:02:55 +0300

    Import all necessary to App.tsx and write JSX to render

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
-import { useState } from "react";
+import { Suspense } from 'react';
+import { MessageComponent } from './components/MessageComponent';
+import { simulateServerRequest } from './utils/simulateServerRequest';
+import { ErrorBoundary } from 'react-error-boundary';
+
+const myMessagePromise = simulateServerRequest('Fake server request', 2000);
 
 export function App() {
-  return <></>;
+  return (
+    <div className="app-wrapper">
+      <h1>React Hooks Homework</h1>
+      <ErrorBoundary
+        fallback={<div className="error-fallback">Something went wrong.</div>}
+      >
+        <Suspense fallback={<div>Loading dynamic data... Please wait.</div>}>
+          <MessageComponent messagePromise={myMessagePromise} />
+        </Suspense>
+      </ErrorBoundary>
+    </div>
+  );
 }
